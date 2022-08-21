956d3dacc4d402a50e228586d8dc283f808157c7
LukeDasios <luke.dasios321@gmail.com>
2022-08-20 21:13:11 -0400

    wip

--- Arrays/MergeOverlappingIntervals.js
+++ Arrays/MergeOverlappingIntervals.js
@@ -1,11 +1,15 @@
 function mergeOverlappingIntervals(array) {
-  let i = 0
-  while (i < array.length - 1) {
-    if (array[i][1] >= array[i+1][0]) {
-      array.splice(i, 2, [array[i][0], array[i+1][1]])
-        i = 0
-      } else i++
-    }
+  array.sort((a, b) => a[0] - b[0])
+  let final = [array[0]]
 
-  return array;
+  for (let i = 1; i < array.length; i++) {
+    let temp = final[final.length-1][1]
+    if (temp >= array[i][0]) {
+      final[final.length-1][1] = Math.max(temp, array[i][1])
+    } else {
+      final.push(array[i])
+    }
+  }
+
+  return final
 }
