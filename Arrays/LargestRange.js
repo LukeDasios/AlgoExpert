3caf317d5068efb6b9e3166f1bd271fa39f1600c
LukeDasios <luke.dasios321@gmail.com>
2022-08-23 21:10:19 -0400

    wip

--- Arrays/LargestRange.js
+++ Arrays/LargestRange.js
@@ -1,30 +1,33 @@
 function largestRange(array) {
-  if (array.length === 1) return [1, 1]
-  array.sort((a, b) => a - b)
-  let hashMap = {}
-  let max = -Infinity
+  // O(n) Time complexity
+  // O(1) Space complexity
 
-  array.push(9000)
-  let last = array[0]
-  let range = 0
-  let x = 0
+  if (array.length === 1) return [1, 1];
+  array.sort((a, b) => a - b);
+  let hashMap = {};
+  let max = -Infinity;
+
+  array.push(9000);
+  let last = array[0];
+  let range = 0;
+  let x = 0;
 
   for (let i = 1; i < array.length; i++) {
-    let temp = array[i]
+    let temp = array[i];
 
     if (temp === last + 1 || temp === last) {
-      range++
+      range++;
     } else {
       if (array[i - range - 1] !== array[i - 1]) {
-        max = Math.max(max, range)
-        hashMap[range] = [array[i - range - 1], array[i - 1]]
+        max = Math.max(max, range);
+        hashMap[range] = [array[i - range - 1], array[i - 1]];
       }
-      range = 0
+      range = 0;
     }
 
-    x = i
-    last = temp
+    x = i;
+    last = temp;
   }
 
-  return hashMap[max]
+  return hashMap[max];
 }
