092f60e7ca3591ecaf75eb41416c7676adc38f0c
LukeDasios <luke.dasios321@gmail.com>
2022-09-08 22:18:26 -0400

    wip

--- Strings/PalindromeCheck.js
+++ Strings/PalindromeCheck.js
@@ -1,3 +1,17 @@
+// Time Complexity: O(n) - where n is the length of the input string
+// Space Complexity: O(1)
+
 function isPalindrome(string) {
-  return string === string.split("").reverse().join("");
+  let p1 = 0, p2 = string.length - 1
+
+ while (p1 < p2) {
+   if (string[p1] === string[p2]) {
+     p1++
+     p2--
+   } else {
+     p1 = 1000
+   }
+ }
+
+ return p1 !== 1000
 }
