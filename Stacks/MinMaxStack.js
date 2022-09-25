38bc607548420705581dde2d77e8fb3f813e470e
LukeDasios <luke.dasios321@gmail.com>
2022-09-25 11:16:44 -0400

    wip

---
+++ Stacks/MinMaxStack.js
@@ -0,0 +1,41 @@
+// All methods are O(n) in Time and Space Complexity
+
+class MinMaxStack {
+  constructor() {
+    this.minMaxStack = [];
+    this.stack = [];
+  }
+
+  peek() {
+    return this.stack[this.stack.length - 1];
+  }
+
+  pop() {
+    this.minMaxStack.pop();
+    return this.stack.pop();
+  }
+
+  push(number) {
+    let newMinMax = {
+      min: number,
+      max: number,
+    };
+
+    if (this.minMaxStack.length) {
+      let lastMinMax = this.minMaxStack[this.minMaxStack.length - 1];
+      newMinMax["min"] = Math.min(lastMinMax["min"], number);
+      newMinMax["max"] = Math.max(lastMinMax["max"], number);
+    }
+
+    this.minMaxStack.push(newMinMax);
+    this.stack.push(number);
+  }
+
+  getMin() {
+    return this.minMaxStack[this.minMaxStack.length - 1]["min"];
+  }
+
+  getMax() {
+    return this.minMaxStack[this.minMaxStack.length - 1]["max"];
+  }
+}
